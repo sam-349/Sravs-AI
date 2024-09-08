5b1001c4761b4f2927ea6534dc69002ef6871d4d
samuel <samuelraj349@gmail.com>
2024-09-08 23:13:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from './components/Main/main';
+import Sidebar from'./components/Sidebar/sidebar'
+function App() {
+  return (
+    <>
+      <Sidebar/>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
